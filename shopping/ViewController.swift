31d2ca1d6d48e5817a7388903147a47163ab7326
黃冠霖 <mac@macs-macbook-pro.local>
2018-07-11 01:53:17 +0800

    first commit

--- shopping/ViewController.swift
+++ shopping/ViewController.swift
@@ -15,6 +15,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var addProduct: UIStepper!
 
+    @IBOutlet weak var addProduct2: UIStepper!
+
+    @IBOutlet weak var addProduct3: UIStepper!
+
+    @IBOutlet weak var addProduct4: UIStepper!
+
+    
+    @IBAction func addCart(_ sender: UIStepper) {
+    
+      
+       
+        countAmt()
+    }
+    
+    @IBOutlet weak var sumAmt: UITextField!
+    
+    func countAmt()
+    {
+        let _addProduct:Int=Int(addProduct.value)
+        let _addProduct2:Int=Int(addProduct2.value)
+        let _addProduct3:Int=Int(addProduct3.value)
+        let _addProduct4:Int=Int(addProduct4.value)
+        var total=_addProduct+_addProduct2+_addProduct3+_addProduct4
+        
+        
+        sumAmt.text=String(total)
+        
+    }
 }
 
